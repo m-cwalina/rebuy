6b1f91116d80cd6ef6c5bb5fe84830bdd4ca309c
Matthew Cwalina <matthewcwalina@gmail.com>
2023-08-08 15:53:32 +0200

    Added symfony serializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,10 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/api/products")
- */
-
+#[Route('/api/products')]
 class ProductController extends AbstractController
 {
     private $entityManager;
@@ -23,29 +20,40 @@ class ProductController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    /**
-     * @Route("/", methods={"GET"})
-     */
-    public function getAll(): JsonResponse
-    {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
-        $data = [];
-        foreach ($products as $product) {
-            $data[] = [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'manufacturer' => $product->getManufacturer(),
-                'price' => $product->getPrice(),
-                // Add more fields as needed
-            ];
+#[Route('', methods: ['GET'])]
+public function index(): JsonResponse
+{
+    $products = $this->entityManager->getRepository(Product::class)->findAll();
+    $data = [];
+    foreach ($products as $product) {
+        // Fetch related categories and EANCodes for each product
+        $categories = $product->getCategories();
+        $categoryNames = [];
+        foreach ($categories as $category) {
+            $categoryNames[] = $category->getCategory();  // assuming getCategory() returns the name or title of the category
         }
-        return new JsonResponse($data);
-    }
 
-    /**
-     * @Route("/{id}", methods={"GET"})
-     */
-    public function getOne(int $id): JsonResponse
+        $eanCodes = $product->getEanCodes();
+        $eanCodeValues = [];
+        foreach ($eanCodes as $eanCode) {
+            $eanCodeValues[] = $eanCode->getCode();  // assuming getCode() returns the EAN code value
+        }
+
+        $data[] = [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'manufacturer' => $product->getManufacturer(),
+            'price' => $product->getPrice(),
+            'categories' => $categoryNames,   // Add categories to the product data
+            'eanCodes' => $eanCodeValues,     // Add EANCodes to the product data
+            // Add more fields as needed
+        ];
+    }
+    return new JsonResponse($data);
+}
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(int $id): JsonResponse
     {
         $product = $this->entityManager->getRepository(Product::class)->find($id);
         if (!$product) {
@@ -61,9 +69,7 @@ class ProductController extends AbstractController
         return new JsonResponse($data);
     }
 
-    /**
-     * @Route("/", methods={"POST"})
-     */
+    #[Route('', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -79,9 +85,7 @@ class ProductController extends AbstractController
         return new JsonResponse(['id' => $product->getId()], Response::HTTP_CREATED);
     }
 
-    /**
-     * @Route("/{id}", methods={"PUT"})
-     */
+    #[Route('/{id}', methods: ['PUT', 'PATCH'])]
     public function update(int $id, Request $request): JsonResponse
     {
         $product = $this->entityManager->getRepository(Product::class)->find($id);
@@ -99,9 +103,7 @@ class ProductController extends AbstractController
         return new JsonResponse(['message' => 'Product updated']);
     }
 
-    /**
-     * @Route("/{id}", methods={"DELETE"})
-     */
+    #[Route('/{id}', methods: ['DELETE'])]
     public function delete(int $id): JsonResponse
     {
         $product = $this->entityManager->getRepository(Product::class)->find($id);
